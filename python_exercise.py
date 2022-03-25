d0fb94fede940413b094aff3c9ffcb779e4a01ec
cgiltner-PS <81636679+cgiltner-PS@users.noreply.github.com>
2022-03-25 10:48:41 -0600

    Create python_exercise.py

---
+++ python_exercise.py
@@ -0,0 +1,39 @@
+import requests
+
+# Write a program that retrieves the names of the astronauts in space and the location of the International Space Station using the following API
+# http://api.open-notify.org/
+
+# You should not need to import any more modules
+
+
+def get_json(url):
+    # Function to get data from URL - optional
+    
+
+def get_astronauts():
+    # Returns a list of all people currently in space
+    
+
+def get_iss_location():
+    # Returns the current latitude and longitude of the ISS
+
+
+def main():
+    # Runs the get astronauts command -- don't edit
+    astronauts_in_space = get_astronauts()
+    
+    # Print count of people in space
+    
+    
+    # Print name of people in space
+    
+
+    # Runs the get location command -- don't edit
+    iss_position = get_iss_location()
+
+    # Print the Latitude and Longitude of the ISS
+
+
+# Don't edit
+if __name__ == "__main__":
+    main()
